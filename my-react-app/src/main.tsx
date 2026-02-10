2f07893e0dcf0269609677c2aa0731984d6e7348
Diane <dianahporter@gmail.com>
2026-02-10 18:19:08 +0200

    feat: rendering html

--- my-react-app/src/main.tsx
+++ my-react-app/src/main.tsx
@@ -1,5 +1,17 @@
 import { createRoot } from 'react-dom/client'
+const myelement = (
+  <table>
+    <tr>
+    <th>Name</th>
+    </tr>
+    <tr>
+      <td>Diane</td>
+    </tr>
+    
+    <tr>
+      <td>Hope</td>
+    </tr>
+  </table>
+);
 
-createRoot(document.getElementById('root')!).render(
-  <h1>Hello React!</h1> 
-)    //! for null 
+createRoot(document.getElementById('root')!).render(myelement)    //! for null 
